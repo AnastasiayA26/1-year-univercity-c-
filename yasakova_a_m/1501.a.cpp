53cb936952b6747729500cfd1f73252b908fe9e3
AnastasiayA26 <75423962+AnastasiayA26@users.noreply.github.com>
2022-10-17 17:37:22 +0300

    Add files via upload

--- yasakova_a_m/1501.a.cpp
+++ yasakova_a_m/1501.a.cpp
@@ -1,28 +1,45 @@
 #include <iostream>
 #include <vector>
 
-int main() {
+int main(){
     int n = 0;
-    std::cin>>n;
-    for(int i = 0; i < n; i+= 1) {
-        int t = 0;
-        int time = 0;
-        std::cin >> t;
-        int k;
-        k = t*2 + t;
-        std::vector<int> a(k);
-        std::cout<<k;
-        for(int j = 0; j < t; j += 1){
-            int tmp;
+    std::cin>> n;
+    for(int elem = 0; elem < n; elem+=1){
+        int total =0;
+        std::cin>>total;
+        std::vector<int> wait(total);
+        std::vector<int> arri(total* 2);
+        std::vector<int> train(total* 2 + 1);
+        for(int time = 0; time < total* 2; time += 1){
+            int tmp = 0;
             std::cin>>tmp;
-            a.push_back(tmp);
+            arri[time] = tmp;
         }
-        time = a[0];
-        for(int r = 1; r <  t * 2 + t; r+=1){
-            time += a[r] - a[r - 1] + a[- t + r];
+        for(int t = 0; t < total; t += 1){
+            int stai = 0;
+            std::cin>>stai;
+            wait[t] = stai;
         }
-        std::cout << time << std::endl;
-    }
-    return 0;
-}
+        int count = wait[0] + arri[0];
+        for(int j = 1; j < total * 2 - 1; j += 1){
+            if(j % 2 == 0) {
+                count += arri[j] - arri[j - 1] + wait[j/2];
+            }
+            else{
+                if((arri[j] - arri[j - 1]) % 2 == 0  && count > arri[j-1]){
+                    count += (arri[j] - arri[j-1])/2;
+                }
+                else {
+                    if ((arri[j] - arri[j - 1]) % 2 == 0 and (arri[j] - count <= ((arri[j] - arri[j - 1]) / 2))) {
+                        count += (arri[j] - arri[j - 1]);
+                    } else {
+                        count += (arri[j] - arri[j - 1]) / 2 + 1;
+                    }
+                }
+            }
 
+        }
+        std::cout<<count<<std::endl;
+    }
+  return 0 ;
+}
