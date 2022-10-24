3e7d3f9ec14e1db62eefe6420d521f5fe712d129
AnastasiayA26 <75423962+AnastasiayA26@users.noreply.github.com>
2022-10-25 00:52:37 +0300

    Add files via upload

--- yasakova_a_m/1669.d.cpp
+++ yasakova_a_m/1669.d.cpp
@@ -5,45 +5,58 @@ int main() {
     int repeat = 0;
     std::cin>>repeat;
     for(int i = 0; i < repeat; i += 1){
+        int lenght = 0;
+        std::cin>>lenght;
         std::string word;
         std::cin>>word;
-        std::string untill;
-        int q = 0;
-        int inde0 = 0;
-        int ind1 = 0;
-        for(int j=word.size() - 1; j >= 0; j -= 1){
-            if(word[j] == '0') {
-                inde0 = j;
-            }
-            if(word[j] == '1'){
-                ind1 = j;
-                break;
-                }
-            if(word[j] == '?'){
-                q += 1;
-            }
-            }
-        if(q == word.size()){
-            std::cout<<q << std::endl;
-        }
-        else {
-            if (ind1 != 0 and inde0 != 0) {
-                std::cout << abs(inde0 - ind1) + 1 << std:: endl;
-            }
-            else{
-                if(word.size() == 1){
-                    std::cout<< "1" << std::endl;
+        std::string untillw;
+        bool flag = true;
+        int r = 0;
+        int b = 0;
+        for(int j=0; j < lenght; j += 1){
+            if(word[j] != 'W'){
+                if(word[j] == 'R'){
+                    r += 1;
+                    untillw += word[j];
                 }
                 else{
-                    if(ind1 != 0 and inde0==0){
-                        std::cout<< word.size()- ind1 << std::endl;
-                    }
-                    else{
-                        std::cout<< inde0 + 1 << std::endl;
-                    }
+                    b += 1;
+                    untillw += word[j];
                 }
+            }
+            if(j != 0 and word[j] == 'W' and (untillw.size() != 0 and untillw.size() != 1) and (r != untillw.size() and b != untillw.size())){
+                untillw.clear();
+                r = 0;
+                b = 0;
+            }
+            else {
+                if (j != 0 and word[j] == 'W' and untillw.size() == 1) {
+                    flag = false;
+                    r = 0;
+                    b = 0;
+                    break;
+                }
+                if(j !=0  and (j != lenght - 1) and word[j] == 'W' and (untillw.size() == r or untillw.size() == b) and untillw.size() != 0){
+                    flag = false;
+                    r = 0;
+                    b = 0;
+                    untillw.clear();
+                }
+            }
+        }
+        if((r == untillw.size() or b == untillw.size()) and untillw.size() != 0){
+            std::cout<<"NO"<<std::endl;
+        }
+        else {
+            if (flag and untillw.size() != 1) {
+                std::cout << "YES" << std::endl;
+                untillw.clear();
+            } else {
+                std::cout << "NO" << std::endl;
+                untillw.clear();
             }
         }
     }
     return 0;
 }
+
