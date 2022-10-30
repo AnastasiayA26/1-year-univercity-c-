afeb1366ada06ed4dd1e913c366845f2578589f3
AnastasiayA26 <75423962+AnastasiayA26@users.noreply.github.com>
2022-10-30 13:17:02 +0300

    Add files via upload

--- yasakova_a_m/1675.c.cpp
+++ yasakova_a_m/1675.c.cpp
@@ -4,65 +4,37 @@
 int main() {
     int repeat = 0;
     std::cin>>repeat;
-    for(int i = 0; i < repeat; i += 1){
-        std::string word;
-        std::cin>>word;
-        std::string untill;
-        int q = 0;
-        int inde0 = 0;
-        int ind1 = 0;
-        int ind1l = 0;
-        int inde0l = 0;
-        for(int j=word.size() - 1; j >= 0; j -= 1){
-            if(word[j] == '0') {
-                inde0l = inde0;
-                inde0 = j;
+    for(int i = 0; i < repeat; i+= 1) {
+        std::string rob;
+        int si = 0;
+        int last1 = -100;
+        int first0 = -100;
+        std::cin>>rob;
+        si = rob.size();
+        for(int j = 0; j < si; j += 1){
+            if(rob[j] == '1' and first0 < -1){
+                last1= j;
             }
-            if(word[j] == '1' and  inde0 != 0){
-                ind1 = j;
+            if(rob[j] == '0'){
+                first0 = j;
                 break;
-                }
-            if(word[j] == '1' and  inde0 == 0 and ind1l == 0){
-                ind1l = j;
             }
-            if(word[j] == '1' and inde0 == 0 and ind1l != 0){
-                ind1 = j;
-            }
-            if(word[j] == '?'){
-                q += 1;
-            }
-            }
-        if(q == word.size()){
-            std::cout<<q << std::endl;
+        }
+        if(last1 > -1 and first0 > -1){
+            std::cout<< first0 - last1 + 1 << std::endl;
         }
         else {
-            if (ind1 != 0 and inde0 != 0) {
-                std::cout << abs(inde0 - ind1) + 1 << std:: endl;
+            if (last1 > -1 and first0 < -1) {
+                std::cout << si - last1 << std::endl;
             }
-            else{
-                if(word.size() == 1){
-                    std::cout<< "1" << std::endl;
-                }
-                else{
-                    if(ind1l != 0 and inde0 == 0){
-                        std::cout<< word.size() - ind1l<< std::endl;
-                    }
-                    else {
-                        if(inde0l != 0){
-                            std::cout<< inde0 + 1 <<std:: endl;
-                        }
-                        else {
-                            if (ind1l == 0 and inde0 == 0) {
-                                std::cout << word.size() - ind1 << std::endl;
-                            } else {
-                                std::cout << inde0 + 1 << std::endl;
-                            }
-                        }
-                    }
+            else {
+                if (last1 < -1 and first0 > -1) {
+                    std::cout << first0 + 1 << std::endl;
+                } else {
+                    std::cout << si << std::endl;
                 }
             }
         }
     }
     return 0;
 }
-
