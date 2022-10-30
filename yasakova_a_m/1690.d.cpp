9af2864aa282fdf074e0362c200deb25d1707ed0
AnastasiayA26 <75423962+AnastasiayA26@users.noreply.github.com>
2022-10-30 15:17:26 +0300

    Add files via upload

--- yasakova_a_m/1690.d.cpp
+++ yasakova_a_m/1690.d.cpp
@@ -1,41 +1,40 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 int main() {
     int repeat = 0;
     std::cin>>repeat;
-    for(int i = 0; i < repeat; i += 1){
-       int lenght = 0;
-       int stripe = 0;
-       std::cin>>lenght>>stripe;
-       int micolor = 100000000;
-       std::string reco;
-       std::cin>>reco;
-       int b = 0;
-       if(lenght == stripe){
-           for(int r = 0; r < stripe; r += 1){
-               if(reco[r] == 'B'){
-                   b += 1;
-               }
-           }
-           micolor = lenght - b;
-       }
-       else {
-           for (int j = 0; j < lenght - stripe + 1; j += 1) {
-               b = 0;
-               for (int t = j; t < stripe + j; t += 1) {
-                   if (reco[t] == 'B') {
-                       b += 1;
-                   }
-               }
-               if (stripe - b < micolor) {
-                   micolor = stripe - b;
-               }
-           }
-       }
-       std::cout<<micolor<<std::endl;
+    for(int i = 0; i < repeat; i+= 1) {
+        std::string rob;
+        int si = 0;
+        int lenght = 0;
+        std::cin >> si >> lenght >> rob;
+        int b = 0;
+        int maxw = 100000;
+        for (int j = 0; j < si - lenght; j += 1) {
+            if (rob[j] == 'B' and rob[j + lenght] == 'W' or rob[j] == 'B' and rob[j + lenght] == 'B' or rob[j] == 'W' and rob[j + lenght] == 'B') {
+                b += 1;
+            } else {
+                    maxw = std::min(maxw, lenght - b);
+                    if (lenght - b < 0) {
+                        maxw = 0;
+                        break;
+                    }
+            }
+        }
+        maxw = std::min(maxw, lenght - b);
+        b = 0;
+        for(int w = 0; w < lenght; w += 1){
+            if(rob[w] == 'B'){
+                b += 1;
+              }
+            }
+        maxw = std::min(maxw, lenght - b);
+        if (maxw < 0) {
+            maxw = 0;
+        }
+            std::cout << maxw << std::endl;
     }
     return 0;
 }
- 
-
